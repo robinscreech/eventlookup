f85640894f97283f692ffa31872ffd1a9349bb37
Robin S <robinscreech4@gmail.com>
2018-07-20 17:25:20 +0100

    send it all as text for fun

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.post('/', function (req, res){
 })
 
 
-app.listen(8000, () => console.log('Example app listening on port 8000!'))
+app.listen(8000, () => console.log('App listening on port 8000!'))
 
 const search = "5b1902b8352f53e06abe87d9"
 const search1 = "5800e33d414fb5dfd13da446"
@@ -19,11 +19,24 @@ const search1 = "5800e33d414fb5dfd13da446"
 
 app.get('/', function(req, res){
 	let results = [];
+	let sendThisData;
 
 	json.events.forEach(function(item, index){
 		if (item.data.id == search){
-			results.push(item.data.title + " - /" + item.data.urlId)
+			//results.push(item.data.title + " - /" + item.data.urlId)
+			// really i want to send all of the data
+			results.push(item.data)
 		}
 	})
-	res.render('index', {title: 'Events LookUp', message:results})
+
+	results.forEach(function(item, index){
+		//sendThisData = item.title
+		console.log(item.title)
+	})
+
+	sendThisData = JSON.stringify(results)
+
+
+	//res.render('index', {title: 'Events LookUp', message:results})
+	res.render('index', {title: 'Events LookUp', message:sendThisData})
 })
